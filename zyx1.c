a4e60d5e9dadbf4135a9398080efaba1eb0dd1d5
Yuxuan <zyx@mwc-026062.dhcp.missouri.edu>
2016-02-14 19:42:09 -0600

    This is the first version of the zyx1.c

---
+++ zyx1.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: zyx
+ *
+ * Created on June 17, 2015, 12:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char** argv) {
+   
+    printf("hello.")
+
+    return (EXIT_SUCCESS);
+}
